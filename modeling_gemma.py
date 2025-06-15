1a7df3ea18c2834bae3ca4fb4654f9af94e8e43e
Yingjun Mou <renzomou@gmail.com>
2025-06-15 16:16:47 -0700

    Add Gemma root mean square normalization module.

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -110,6 +110,24 @@ class PaliGemmaConfig():
         self.vision_config.projection_dim = projection_dim
 
 
+class GemmaRMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.zeros(dim))
+
+    # Add a small number inside the square root denominator to avoid dividing by zero.
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float())
+        # Llama does x.to(float16) * w whilst Gemma is (x * w).to(float16)
+        # See https://github.com/huggingface/transformers/pull/29402
+        output = output * (1.0 + self.weight.float())
+        return output.type_as(x)
+
+
 class GemmaModel(nn.Module):
 
     def __init__(self, config: GemmaConfig):
