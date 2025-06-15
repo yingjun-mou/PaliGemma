86a3ca8455024ff926047158a945956adcf6c369
Yingjun Mou <renzomou@gmail.com>
2025-06-15 13:26:36 -0700

    Layout the overall config structure for PaliGemma.

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -1,8 +1,42 @@
 """This module corresponds to the Gemma language model portion in Figure 1 of the PaliGemma paper. Source: https://arxiv.org/abs/2407.07726."""
 
 from typing import Optional, Tuple
-from modeling_siglip import SiglipVisionModel
+from modeling_siglip import SiglipVisionConfig, SiglipVisionModel
 import torch
+
+
+class PaliGemmaConfig():
+    """The config passed into PaliGemma - the final model consists of both SigLIP and Gemma."""
+    
+    def __init__(
+            self,
+            vision_config=None, # passed into SigLIP
+            text_config=None, # passed into Gemma
+            ignore_index=100,
+            image_token_index=25600, # the index for placholder token <image>
+            vocab_size=257152,
+            projection_dim=2048, # the dimension of image embedding came out of the linear layer
+            hidden_size=2048,
+            pad_token_id=None,
+            **kwargs,
+    ):
+        super().__init__()
+        self.ignore_index = ignore_index
+        self.image_token_index = image_token_index
+        self.vocab_size = vocab_size
+        self.projection_dim = projection_dim
+        self.hidden_size = hidden_size
+        self.vision_config = vision_config
+        self.is_encoder_decoder = False
+        self.pad_token_id = pad_token_id
+
+        self.vision_config = SiglipVisionConfig(**vision_config)
+        self.text_config = GemmaConfig(**text_config, pad_token_id=pad_token_id)
+
+        self.vocab_size = self.text_config.vocab_size
+
+        self.text_config.num_image_tokens = (self.vision_config.image_size // self.vision_config.patch_size)
+        self.vision_config.projection_dim = projection_dim
 
 
 class PaliGemmaForConditionalGeneration(nn.Module):
