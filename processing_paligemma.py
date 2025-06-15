7c157073148d6ca05058443a68485c7617780c73
Yingjun Mou <renzomou@gmail.com>
2025-06-15 10:33:27 -0700

    Implement process_image logic in processing_paligemma module.

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -1,8 +1,78 @@
 
-from typing import List
+from typing import Dict, List, Optional, Union, Tuple, Iterable
 import numpy as np
 from PIL import Image
 import torch
+
+IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
+IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
+
+
+def resize(
+    image: Image,
+    size: Tuple[int, int],
+    resample: Image.Resampling = None,
+    reducing_gap: Optional[int] = None,
+) -> np.ndarray:
+    height, width = size
+    resized_image = image.resize(
+        (width, height), resample=resample, reducing_gap=reducing_gap
+    )
+    return resized_image
+
+
+def rescale(
+    image: np.ndarray,
+    scale: float,
+    dtype: np.dtype = np.float32
+) -> np.ndarray:
+    rescaled_image = image * scale
+    # Cast to float32
+    rescaled_image = rescaled_image.astype(dtype)
+    
+    return rescaled_image
+
+
+def normalize(
+        image: np.ndarray,
+        mean: Union[float, Iterable[float]],
+        std: Union[float, Iterable[float]],
+) -> np.ndarray:
+    mean = np.array(mean, dtype=image.dtype)
+    std = np.array(std, dtype=image.dtype)
+    image = (image - mean) /std
+    return image
+
+
+def process_images(
+        images: List[Image.Image],
+        size: Dict[str, int] = None,
+        resample: Image.Resampling = None,
+        rescale_factor: float = None,
+        image_mean: Optional[Union[float, List[float]]] = None,
+        image_std: Optional[Union[float, List[float]]] = None,
+) -> List[np.ndarray]:
+    """This method does 4 things:
+    1. resize image
+    2. rescale pixel values to be in the range [0, 1]
+    3. normalize the pixel values to have mean 0 and standard deviation 1
+    4. transpose channel dimension to the first dimension, because the model expects [Channel, Height, Width]
+    """
+    height, width = size[0], size[1]
+    # Resize
+    images = [
+        resize(image=image, size=(height, width), resample=resample) for image in images
+    ]
+    # Convert each image to a numpy array
+    images = [np.array(image) for image in images]
+    # Rescale
+    images = [rescale(image, scale=rescale_factor) for image in images]
+    # Normalize
+    images = [normalize(image, mean=image_mean, std=image_std) for image in images]
+    # Transpose
+    images = [image.transpose(2, 0, 1) for image in images]
+
+    return images
 
 
 class PaliGemmaProcessor:
